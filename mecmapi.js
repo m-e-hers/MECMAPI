81ed96b41aeae5f23fb45f8dd1d9f1c249b62b5a
Kanischev <Vladimir.Kanischev@softline.com>
2022-04-27 13:38:18 +0300

    t2

--- mecmapi.js
+++ mecmapi.js
@@ -2,16 +2,27 @@ const mecmapi = {
 	test: ()=>{alert ("LOADED 2");}
 };
 
+// if the module has no dependencies, the above pattern can be simplified to
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define([], factory);
+    } else if (typeof module === 'object' && module.exports) {
+        // Node. Does not work with strict CommonJS, but
+        // only CommonJS-like environments that support module.exports,
+        // like Node.
+        module.exports = factory();
+    } else {
+        // Browser globals (root is window)
+        root.returnExports = factory();
+  }
+}(typeof self !== 'undefined' ? self : this, function () {
+
+    // Just return a value to define the module export.
+    // This example returns an object, but the module
+    // can return a function as the exported value.
+    return mecmapi;
+}));
 
 
-export default mecmapi;
-/*
-
-
-
-
-
-
-
-
-*/
+//SOURCE: https://github.com/umdjs/umd/blob/master/templates/returnExports.js
